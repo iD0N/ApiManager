76ef5b7c4e581958217d45df044b716a7e91d8b0
Don <napoleon.bonaparte2012@yahoo.com>
2020-02-05 02:08:31 +0330

    Initial Commit

---
+++ ApiManager/ApiManager.h
@@ -0,0 +1,19 @@
+//
+//  ApiManager.h
+//  ApiManager
+//
+//  Created by Don on 2/5/20.
+//  Copyright © 2020 Don. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApiManager.
+FOUNDATION_EXPORT double ApiManagerVersionNumber;
+
+//! Project version string for ApiManager.
+FOUNDATION_EXPORT const unsigned char ApiManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApiManager/PublicHeader.h>
+
+
